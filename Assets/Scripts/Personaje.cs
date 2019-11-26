0452cbc2706ee5f432bc896fe1ba2f8f91abf762
Sofia Brown <ruthsofiab@gmail.com>
2019-11-26 13:27:20 -0600

    init

---
+++ Assets/Scripts/Personaje.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float vida = 3;
+    public UIManager ui;
+    public int monedas = 0; 
+    
+
+    public void SumarVida(float vidaAdicional)
+    {
+        vida = vida + vidaAdicional; 
+    }
+
+    public void RestarVida(float dano)
+    {
+        vida = vida - dano;
+
+        if(vida < 0.5f)
+        {
+            //Ya esta muerto
+            ui.PrenderPantallaGameOver();
+        }
+    }
+    
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //nos permite acceder a variables de UI
+
+
+public class UIManager : MonoBehaviour
+{
+    //Mostrar vida de personaje - 
+
+    //Mostrar powerup activo
+
+    //Menu inicio, prob. aparte
+
+    //pantalla game over -
+
+    //INTRO SCRIPT: VARIABLES, FUNCIONES, OBJETO
+    public GameObject pantallaGameOver;
+
+    public Image[] vidas;
+    public Sprite vidaLlena;
+    public Sprite vidaMitad;
+    public Sprite vidaVacia;
+    public Personaje personaje;
+
+    //-- funciones de monobehaviour
+    private void Start()
+    {
+        //apagar desde un inicio la pantalla de gameover
+        ApagarPantallaGO(); 
+    }
+
+    //---Funciones propias
+
+    //Funcion == accion
+    public void PrenderPantallaGameOver()
+    {
+        pantallaGameOver.SetActive(true);
+    }
+
+    public void ApagarPantallaGO()
+    {
+        pantallaGameOver.SetActive(false);
+    }
+   
+    public void RefrescarVida()
+    {
+        if(personaje.vida >= 3)
+        {
+            //Llenar todas las vidas
+            vidas[0].sprite = vidaLlena;
+            vidas[1].sprite = vidaLlena;
+            vidas[2].sprite = vidaLlena;
+        }
+        else if(personaje.vida >= 2.5f)
+        {
+            vidas[0].sprite = vidaLlena;
+            vidas[1].sprite = vidaLlena;
+            vidas[2].sprite = vidaMitad;
+        }
+        else if(personaje.vida >= 2.0f)
+        {
+            vidas[0].sprite = vidaLlena;
+            vidas[1].sprite = vidaLlena;
+            vidas[2].sprite = vidaVacia;
+        }
+        else if(personaje.vida >= 1.5f)
+        {
+            vidas[0].sprite = vidaLlena;
+            vidas[1].sprite = vidaMitad;
+            vidas[2].sprite = vidaVacia;
+        }
+        else if(personaje.vida >= 1.0f)
+        {
+            vidas[0].sprite = vidaLlena;
+            vidas[1].sprite = vidaVacia;
+            vidas[2].sprite = vidaVacia;
+        }
+        else if(personaje.vida >= 0.5f)
+        {
+            vidas[0].sprite = vidaMitad;
+            vidas[1].sprite = vidaVacia;
+            vidas[2].sprite = vidaVacia;
+        }
+        else
+        {
+            vidas[0].sprite = vidaVacia;
+            vidas[1].sprite = vidaVacia;
+            vidas[2].sprite = vidaVacia;
+
+        }
+
+
+    }
+
+}
